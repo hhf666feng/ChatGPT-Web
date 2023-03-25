695f1c133ba4fbbd695954a4f649279f3bb0e0a0
tanfan <hhf666feng@163.com>
2023-03-25 13:20:36 +0800

    fix: database env

--- service/src/utils/database.ts
+++ service/src/utils/database.ts
@@ -1,11 +1,14 @@
 import mysql from 'mysql'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const db = mysql.createConnection({
-  host: 'sh-cynosdbmysql-grp-5b6kxbeg.sql.tencentcdb.com',
-  port: 22084,
-  user: 'we_gpt_chat',
-  password: '9azsJGkDssz78aFh',
-  database: 'chat',
+  host: process.env.MYSQL_HOTST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 })
 db.connect()
 export default db
