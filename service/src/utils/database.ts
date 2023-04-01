8333cc3beb35cfc7d949a23e52203022c8a5bc8a
tanfan <hhf666feng@163.com>
2023-04-01 19:01:41 +0800

    feat: addChatLogs debug

--- service/src/utils/database.ts
+++ service/src/utils/database.ts
@@ -1,14 +1,17 @@
+import console from 'console'
 import mysql from 'mysql'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
-
-const db = mysql.createConnection({
+console.log('Connecting to database...')
+const cfg = {
   host: process.env.MYSQL_HOTST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-})
+}
+console.log(cfg)
+const db = mysql.createConnection(cfg)
 db.connect()
 export default db
